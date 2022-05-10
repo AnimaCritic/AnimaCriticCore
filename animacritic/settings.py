2be41de086bc6ba5ae3bf96b266557608e713d86
Gustavo Henrique <ghco@cesar.school>
2022-05-09 21:26:01 -0300

    test coverage 100%

--- animacritic/settings.py
+++ animacritic/settings.py
@@ -13,11 +13,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
-'''
+
 if '/animacritic' in os.environ['HOME']:
     import django_heroku
     django_heroku.settings(locals())
-'''
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

416432ba0d76f20cdf4040096730b58374d521dc
gcosta2107 <garragr10@gmail.com>
2022-06-05 14:01:38 -0300

    linux deploy

--- animacritic/settings.py
+++ animacritic/settings.py
@@ -16,7 +16,9 @@ import os
 if '/animacritic' in os.path.expanduser('~'):
     import django_heroku
     django_heroku.settings(locals())
-
+elif '/animacritic' in os.environ['HOME']:
+    import django_heroku
+    django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

0a30b4df0a88cabf26fedcb1e160769875a8acec
Gustavo Henrique <ghco@cesar.school>
2022-06-06 03:07:12 -0300

    heroku fix 2

--- animacritic/settings.py
+++ animacritic/settings.py
@@ -14,14 +14,9 @@ from pathlib import Path
 import django_heroku
 import os
 
-if '/animacritic' in os.path.expanduser('~'):
-    import django_heroku
-    django_heroku.settings(locals())
-    
 if '/animacritic' in os.environ['HOME']:
     import django_heroku
     django_heroku.settings(locals())
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
